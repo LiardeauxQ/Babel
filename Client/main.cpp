ab563c09441e0f336a5e25777df4189817bb0b07
Quentin Liardeaux <quentin.liardeaux@epitech.eu>
2019-09-09 14:28:06 +0200

    :sparkles: Updating main for test.

--- Client/main.cpp
+++ Client/main.cpp
@@ -6,18 +6,39 @@
 #include "SoundStream.hpp"
 #include <iostream>
 
+int callBack(const void *input, void *output, unsigned long frameCount,
+             const PaStreamCallbackTimeInfo *timeInfo,
+             PaStreamCallbackFlags statusFlags, void *userData) {
+  const auto *testInput = static_cast<const float *>(input);
+  auto *testOutput = static_cast<float *>(output);
+
+  std::cout << "Actual time: " << timeInfo->currentTime << std::endl;
+
+  for (unsigned long i = 0; i < frameCount; i += 2) {
+    testOutput[i] = 1.0;
+    testOutput[i + 1] = -1.0;
+  }
+
+  return paContinue;
+}
+
 int main(int argc, char *argv[]) {
   try {
     AudioController audioController;
 
-    auto devices = audioController.getDevicesInfo();
+    auto defaultDevice = audioController.getDefaultOutputDevice();
 
-    for (auto dev: devices) {
-      std::cout << dev->name << std::endl;
-    }
+    std::cout << "Name: " << defaultDevice->name << std::endl;
 
-    SoundStream stream();
+    auto stream = audioController.createStreamFromDevice(
+        defaultDevice, callBack, nullptr, "Default input stream.");
+
+    stream.start();
+
+    audioController.sleep(100000);
+
+    stream.stop();
   } catch (const AudioControllerError &e) {
     std::cerr << "Cannot create controller " << e.what() << std::endl;
-  }
+  };
 }
