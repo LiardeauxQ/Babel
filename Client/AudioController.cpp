50d191945dc854d26497d1ca42475ebdcb44941b
Quentin Liardeaux <quentin.liardeaux@epitech.eu>
2019-09-18 23:54:48 +0200

    :art: Coding style.

--- Client/AudioController.cpp
+++ Client/AudioController.cpp
@@ -5,21 +5,24 @@
 #include "AudioController.hpp"
 #include <iostream>
 
-#define SAMPLE_RATE (44100)     // TODO Add this to configuration.
+#define SAMPLE_RATE (44100) // TODO Add this to configuration.
 #define FRAME_PER_BUFFER (1024) // TODO Add this to configuration.
 
-inline void handleError(int error) {
+inline void handleError(int error)
+{
     if (error != paNoError)
         throw AudioControllerError(error);
 }
 
-AudioController::AudioController() {
+AudioController::AudioController()
+{
     int error = Pa_Initialize();
 
     handleError(error);
 }
 
-AudioController::~AudioController() {
+AudioController::~AudioController()
+{
     int error = Pa_Terminate();
 
     if (error != paNoError)
@@ -27,7 +30,8 @@ AudioController::~AudioController() {
                   << Pa_GetErrorText(error) << std::endl;
 }
 
-const PaDeviceInfo *AudioController::getDefaultInputDevice() const {
+const PaDeviceInfo* AudioController::getDefaultInputDevice() const
+{
     int idx = Pa_GetDefaultInputDevice();
 
     if (idx == paNoDevice)
@@ -35,7 +39,8 @@ const PaDeviceInfo *AudioController::getDefaultInputDevice() const {
     return Pa_GetDeviceInfo(idx);
 }
 
-const PaDeviceInfo *AudioController::getDefaultOutputDevice() const {
+const PaDeviceInfo* AudioController::getDefaultOutputDevice() const
+{
     int idx = Pa_GetDefaultOutputDevice();
 
     if (idx == paNoDevice)
@@ -45,13 +50,15 @@ const PaDeviceInfo *AudioController::getDefaultOutputDevice() const {
 
 int AudioController::getVersion() { return Pa_GetVersion(); }
 
-std::string AudioController::getTextVersion() {
+std::string AudioController::getTextVersion()
+{
     return std::string(Pa_GetVersionText());
 }
 
-std::vector<const PaDeviceInfo *> AudioController::getDevicesInfo() const {
+std::vector<const PaDeviceInfo*> AudioController::getDevicesInfo() const
+{
     int count = Pa_GetDeviceCount();
-    std::vector<const PaDeviceInfo *> devices;
+    std::vector<const PaDeviceInfo*> devices;
 
     devices.reserve(count);
     for (int i = 0; i < count; ++i)
@@ -63,35 +70,43 @@ std::vector<const PaDeviceInfo *> AudioController::getDevicesInfo() const {
 #pragma clang diagnostic push
 #pragma ide diagnostic ignored "MemberFunctionCanBeStatic"
 
-SoundStream AudioController::createStream(
-        const PaDeviceInfo *dev, PaStreamCallback *callback, void *linkedData,
-        const std::string &name) {
+SoundStream AudioController::createStream(const PaDeviceInfo* dev, PaStreamCallback* callback, void* linkedData,
+    const std::string& name)
+{
+
     return SoundStream(dev->defaultSampleRate, dev->maxInputChannels,
-                       dev->maxOutputChannels, paFloat32,
-                       64, callback, linkedData,
-                       name); // TODO Change framePerBuffer
+        dev->maxOutputChannels, paFloat32,
+        64, callback, linkedData,
+        name);
 }
 
-SoundStream AudioController::createCustomStream(PaStreamParameters *outputStream, PaStreamParameters *inputStream,
-                                                PaStreamCallback *callback, void *userData, PaStreamFlags flags,
-                                                const std::string &name) {
+SoundStream AudioController::createCustomStream(PaStreamParameters* outputStream, PaStreamParameters* inputStream,
+    PaStreamCallback* callback, void* userData, PaStreamFlags flags,
+    const std::string& name)
+{
     return SoundStream(outputStream, inputStream, SAMPLE_RATE,
-                       64, flags, callback, userData,
-                       name);
+        64, flags, callback, userData,
+        name);
 }
 
-int AudioController::getDefaultOutputId() const {
+int AudioController::getDefaultOutputId() const
+{
     return Pa_GetDefaultOutputDevice();
 }
 
-int AudioController::getDefaultInputId() const {
+int AudioController::getDefaultInputId() const
+{
     return Pa_GetDefaultInputDevice();
 }
 
 #pragma clang diagnostic pop
 
-const char *AudioControllerError::what() const noexcept {
+const char* AudioControllerError::what() const noexcept
+{
     return Pa_GetErrorText(error_);
 }
 
-AudioControllerError::AudioControllerError(int error) : error_(error) {}
+AudioControllerError::AudioControllerError(int error)
+    : error_(error)
+{
+}
--- Client/AudioController.hpp
+++ Client/AudioController.hpp
@@ -13,44 +13,48 @@
 
 class AudioController {
 public:
-  // Initialize the AudioController can fail and throw `AudioControllerError`.
-  AudioController();
-  ~AudioController();
+    // Initialize the AudioController can fail and throw `AudioControllerError`.
+    AudioController();
 
-  // Get number version of the program.
-  static int getVersion();
+    ~AudioController();
 
-  // Get text version of the program.
-  static std::string getTextVersion();
+    // Get number version of the program.
+    static int getVersion();
 
-  [[nodiscard]] std::vector<const PaDeviceInfo *> getDevicesInfo() const;
+    // Get text version of the program.
+    static std::string getTextVersion();
 
-    SoundStream createStream(const PaDeviceInfo *dev,
-                             PaStreamCallback *callback,
-                             void *linkedData,
-                             const std::string &name);
+    [[nodiscard]] std::vector<const PaDeviceInfo*> getDevicesInfo() const;
 
-    SoundStream createCustomStream(PaStreamParameters *outputStream,
-                                   PaStreamParameters *inputStream,
-                                   PaStreamCallback *callback, void *userData,
-                                   PaStreamFlags flags, const std::string &name);
+    SoundStream createStream(const PaDeviceInfo* dev,
+        PaStreamCallback* callback,
+        void* linkedData,
+        const std::string& name);
 
-  [[nodiscard]] const PaDeviceInfo *getDefaultInputDevice() const;
-  [[nodiscard]] const PaDeviceInfo *getDefaultOutputDevice() const;
-  [[nodiscard]] int getDefaultOutputId() const;
-  [[nodiscard]] int getDefaultInputId() const;
+    SoundStream createCustomStream(PaStreamParameters* outputStream,
+        PaStreamParameters* inputStream,
+        PaStreamCallback* callback, void* userData,
+        PaStreamFlags flags, const std::string& name);
 
-  inline void sleep(long ms) { Pa_Sleep(ms); }
+    [[nodiscard]] const PaDeviceInfo* getDefaultInputDevice() const;
+
+    [[nodiscard]] const PaDeviceInfo* getDefaultOutputDevice() const;
+
+    [[nodiscard]] int getDefaultOutputId() const;
+
+    [[nodiscard]] int getDefaultInputId() const;
+
+    inline void sleep(long ms) { Pa_Sleep(ms); }
 };
 
 class AudioControllerError : std::exception {
 public:
-  explicit AudioControllerError(int error);
+    explicit AudioControllerError(int error);
 
-  [[nodiscard]] const char *what() const noexcept final;
+    [[nodiscard]] const char* what() const noexcept final;
 
 private:
-  int error_;
+    int error_;
 };
 
 #endif // BABEL_AUDIO_HPP
