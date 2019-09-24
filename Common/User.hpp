9ee70beafb6442f5784929d3dfc717c7b717d92f
Quentin Liardeaux <quentin.liardeaux@epitech.eu>
2019-09-24 17:17:15 +0200

    :sparkles: Working User.

--- Common/User.hpp
+++ Common/User.hpp
@@ -11,76 +11,31 @@
 
 class User {
 public:
-    User(std::string name, const std::string& password)
-        : name_(std::move(name))
-        , password_(name)
-        , id_(0)
-    {
-    }
-
     // Create user from connection and username.
-    User(const Database& connection, std::string username)
-        : name_(std::move(username))
-        , password_()
-        , id_(0)
-    {
-    }
+    User(Database& connection, const std::string& username);
 
     // Create user from connection and id.
-    User(Database& connection, const int id)
-        : name_()
-        , password_()
-        , id_(id)
-    {
-        callbackData data{
-            .name = name_,
-            .password = password_,
-            .used = false,
-        };
-
-        connection.exec("SELECT * FROM `users` WHERE id=" + std::to_string(id), takeUser, &data);
-    }
+    User(Database& connection, int id);
 
     User(const User&) = delete;
 
     ~User() = default;
 
-    [[nodiscard]] const std::string& getName() const
-    {
-        return name_;
-    }
-
-    [[nodiscard]] size_t getId() const
-    {
-        return id_;
-    }
-
-    [[nodiscard]] const std::string& getPassword() const
-    {
-        return password_;
-    }
-
 private:
-    static int takeUser(void* toChange, int argc, char** argv, char** azColName)
-    {
-        auto data = static_cast<callbackData *>(toChange);
-
-        data->used = true;
-        data->password = std::string(argv[0]);
-        data->name = std::string(argv[1]);
-
-        return 0;
-    }
+    static int takeUser(void* toChange, int argc, char** argv, char** azColName);
 
     struct callbackData {
-        std::string &name;
-        std::string &password;
+        std::string& name;
+        std::string& password;
+        size_t& id;
         bool used;
     };
 
     std::string name_;
     std::string password_;
     size_t id_;
+
+    friend std::ostream& operator<<(std::ostream& os, const User& user);
 };
 
 #endif //BABEL_SERVER_USER_HPP
