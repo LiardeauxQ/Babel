127b5895bef8b9bacbbc05703aac0e3b8b02c05c
Alexandre Fourcat <alexandre.fourcat@epitech.eu>
2019-09-24 11:10:43 +0200

    :zap: Updating server main to test database.

--- Server/main.cpp
+++ Server/main.cpp
@@ -7,6 +7,7 @@
 
 #include "ServerApplication.hpp"
 #include "ServerConfig.hpp"
+#include "User.hpp"
 #include <iostream>
 
 int main(int argc, char* argv[])
@@ -20,6 +21,12 @@ int main(int argc, char* argv[])
         ServerApplication server(config);
 
         auto conn = server.getConnection();
+
+        User user(conn, 2);
+
+        std::cout << "username: " << user.getName() << std::endl;
+        std::cout << "password: " << user.getPassword() << std::endl;
+        std::cout << "id: " << user.getId() << std::endl;
 
     } catch (const std::exception& e) {
         std::cerr << "Server failed: " << e.what() << std::endl;
