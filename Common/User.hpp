9da9896e4fbd7afb0fc954774664a12b6c488aa2
Alexandre Fourcat <alexandre.fourcat@epitech.eu>
2019-09-24 11:10:07 +0200

    :sparkles: Adding User class.

--- Common/User.hpp
+++ Common/User.hpp
@@ -24,22 +24,60 @@ public:
         , password_()
         , id_(0)
     {
-
     }
 
     // Create user from connection and id.
-    User(const Database& connection, const int id)
+    User(Database& connection, const int id)
         : name_()
         , password_()
         , id_(id)
     {
+        callbackData data{
+            .name = name_,
+            .password = password_,
+            .used = false,
+        };
+
+        connection.exec("SELECT * FROM `users` WHERE id=" + std::to_string(id), takeUser, &data);
     }
 
     User(const User&) = delete;
 
     ~User() = default;
 
+    [[nodiscard]] const std::string& getName() const
+    {
+        return name_;
+    }
+
+    [[nodiscard]] size_t getId() const
+    {
+        return id_;
+    }
+
+    [[nodiscard]] const std::string& getPassword() const
+    {
+        return password_;
+    }
+
 private:
+    static int takeUser(void* toChange, int argc, char** argv, char** azColName)
+    {
+        auto data = static_cast<callbackData *>(toChange);
+
+        data->used = true;
+        data->password = std::string(argv[0]);
+        data->name = std::string(argv[1]);
+
+        return 0;
+    }
+
+    struct callbackData {
+        std::string &name;
+        std::string &password;
+        bool used;
+    };
+
     std::string name_;
     std::string password_;
     size_t id_;
