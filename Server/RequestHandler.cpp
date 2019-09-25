653a762023c1f583431a528e626823947d026a2d
Alexandre Fourcat <alexandre.fourcat@epitech.eu>
2019-09-25 12:46:01 +0200

    :sparkles: Adding RequestHandler architecture.

--- Server/RequestHandler.cpp
+++ Server/RequestHandler.cpp
@@ -3,3 +3,23 @@
 //
 
 #include "RequestHandler.hpp"
+
+void RequestHandler::handleRequest(Message& request)
+{
+    switch (request.getId()) {
+    case CLIENT_HELLO:
+        hello((client_hello_t*)request.payload());
+    case CLIENT_PING:
+        ping((client_ping_t*)request.payload());
+    case CLIENT_ACCEPT_FRIEND:
+        acceptFriend((client_accept_friend_t*)request.payload());
+    case CLIENT_FRIEND_REQUEST:
+        friendRequest((client_friend_request_t*)request.payload());
+    case CLIENT_BYE:
+        bye((client_bye_t*)request.payload());
+    case CLIENT_CALL:
+        call((client_call_t*)request.payload());
+    default:
+        throw "Unknown request.";
+    }
+}
--- Server/RequestHandler.hpp
+++ Server/RequestHandler.hpp
@@ -5,7 +5,34 @@
 #ifndef BABEL_SERVER_REQUESTHANDLER_HPP
 #define BABEL_SERVER_REQUESTHANDLER_HPP
 
+#include "protocol.h"
+#include "Message.hpp"
+
 class RequestHandler {
+public:
+    RequestHandler() = delete;
+    RequestHandler(const RequestHandler&) = delete;
+    ~RequestHandler() = delete;
+
+    static void handleRequest(const Message& request);
+
+    static void friendRequest(client_friend_request_t *payload);
+
+    static void ping(client_ping_t *payload);
+
+    static void hello(client_hello_t *payload);
+
+    static void goodbye(client_goodbye_t *payload);
+
+    static void clientRegister(client_register_t *payload);
+
+    static void call(client_call_t *payload);
+
+    static void bye(client_bye_t *);
+
+    static void acceptFriend(client_accept_friend_t *payload);
+
+    static void friendStatus(client_friend_status_t *);
 };
 
 #endif //BABEL_SERVER_REQUESTHANDLER_HPP
