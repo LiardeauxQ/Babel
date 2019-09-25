26cf85e4f295823eceef5318f8e084f4710cc62b
Thomas nwmqpa Nicollet <thomas.nicollet@epitech.eu>
2019-09-25 12:44:49 +0200

    :zap: Adding RequestHandler.

---
+++ Server/RequestHandler.cpp
@@ -0,0 +1,5 @@
+//
+// Created by alex on 9/25/19.
+//
+
+#include "RequestHandler.hpp"
---
+++ Server/RequestHandler.hpp
@@ -0,0 +1,11 @@
+//
+// Created by alex on 9/25/19.
+//
+
+#ifndef BABEL_SERVER_REQUESTHANDLER_HPP
+#define BABEL_SERVER_REQUESTHANDLER_HPP
+
+class RequestHandler {
+};
+
+#endif //BABEL_SERVER_REQUESTHANDLER_HPP
