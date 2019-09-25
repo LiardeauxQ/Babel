238cc2fa87b7c63ae728138daa33840fba2aabf6
Alexandre Fourcat <alexandre.fourcat@epitech.eu>
2019-09-25 18:41:29 +0200

    :hammer: Rework RequestHandler with SharedData.

--- Server/RequestHandler.cpp
+++ Server/RequestHandler.cpp
@@ -3,63 +3,85 @@
 //
 
 #include "RequestHandler.hpp"
+#include <boost/bind.hpp>
 
-void RequestHandler::handleRequest(Message& request)
+void RequestHandler::handleRequest(Message& request, SharedData& data)
 {
     switch (request.getId()) {
     case CLIENT_HELLO:
-        hello((client_hello_t*)request.payload());
+        hello((client_hello_t*)request.getPayload(), data);
+        break;
     case CLIENT_PING:
-        ping((client_ping_t*)request.payload());
+        ping((client_ping_t*)request.getPayload(), data);
+        break;
     case CLIENT_ACCEPT_FRIEND:
-        acceptFriend((client_accept_friend_t*)request.payload());
+        acceptFriend((client_accept_friend_t*)request.getPayload(), data);
+        break;
     case CLIENT_FRIEND_REQUEST:
-        friendRequest((client_friend_request_t*)request.payload());
+        friendRequest((client_friend_request_t*)request.getPayload(), data);
+        break;
     case CLIENT_BYE:
-        bye((client_bye_t*)request.payload());
+        bye((client_bye_t*)request.getPayload(), data);
+        break;
     case CLIENT_CALL:
-        call((client_call_t*)request.payload());
+        call((client_call_t*)request.getPayload(), data);
+        break;
     case CLIENT_GOODBYE:
-        goodbye((client_goodbye_t*)request.payload());
+        goodbye((client_goodbye_t*)request.getPayload(), data);
+        break;
     case CLIENT_REGISTER:
-        clientRegister((client_register_t*)request.payload());
+        clientRegister((client_register_t*)request.getPayload(), data);
+        break;
     default:
         throw "Unknown request.";
     }
 }
 
-void RequestHandler::ping(client_ping_t* payload)
+void RequestHandler::ping(client_ping_t* payload, SharedData& data)
 {
 }
 
-void RequestHandler::hello(client_hello_t* payload)
+static void callbackWrite(const boost::system::error_code& ec)
+{
+    if (ec)
+        std::cerr << ec.message() << std::endl;
+}
+
+void RequestHandler::hello(client_hello_t* payload, SharedData& data)
+{
+    std::cout << "Username: " << payload->username << std::endl;
+    std::cout << "Password: " << payload->password << std::endl;
+
+    boost::asio::async_write(
+        data.socket,
+        boost::asio::buffer(std::string("Salut maman, je passe allah tele!")),
+        boost::bind(callbackWrite, boost::asio::placeholders::error));
+}
+
+void RequestHandler::friendRequest(client_friend_request_t* payload, SharedData& data)
 {
 }
 
-void RequestHandler::friendRequest(client_friend_request_t* payload)
+void RequestHandler::goodbye(client_goodbye_t* payload, SharedData& data)
 {
 }
 
-void RequestHandler::goodbye(client_goodbye_t* payload)
+void RequestHandler::clientRegister(client_register_t* payload, SharedData& data)
 {
 }
 
-void RequestHandler::clientRegister(client_register_t* payload)
+void RequestHandler::call(client_call_t* payload, SharedData& data)
 {
 }
 
-void RequestHandler::call(client_call_t* payload)
+void RequestHandler::bye(client_bye_t*, SharedData& data)
 {
 }
 
-void RequestHandler::bye(client_bye_t*)
+void RequestHandler::acceptFriend(client_accept_friend_t* payload, SharedData& data)
 {
 }
 
-void RequestHandler::acceptFriend(client_accept_friend_t* payload)
-{
-}
-
-void RequestHandler::friendStatus(client_friend_status_t*)
+void RequestHandler::friendStatus(client_friend_status_t*, SharedData& data)
 {
 }
--- Server/RequestHandler.hpp
+++ Server/RequestHandler.hpp
@@ -6,6 +6,7 @@
 #define BABEL_SERVER_REQUESTHANDLER_HPP
 
 #include "protocol.h"
+#include "SharedData.hpp"
 #include "Message.hpp"
 
 class RequestHandler {
@@ -14,25 +15,25 @@ public:
     RequestHandler(const RequestHandler&) = delete;
     ~RequestHandler() = delete;
 
-    static void handleRequest(Message& request);
+    static void handleRequest(Message& request, SharedData& data);
 
-    static void friendRequest(client_friend_request_t *payload);
+    static void friendRequest(client_friend_request_t *payload, SharedData& data);
 
-    static void ping(client_ping_t *payload);
+    static void ping(client_ping_t *payload, SharedData& data);
 
-    static void hello(client_hello_t *payload);
+    static void hello(client_hello_t *payload, SharedData& data);
 
-    static void goodbye(client_goodbye_t *payload);
+    static void goodbye(client_goodbye_t *payload, SharedData& data);
 
-    static void clientRegister(client_register_t *payload);
+    static void clientRegister(client_register_t *payload, SharedData& data);
 
-    static void call(client_call_t *payload);
+    static void call(client_call_t *payload, SharedData& data);
 
-    static void bye(client_bye_t *);
+    static void bye(client_bye_t *, SharedData& data);
 
-    static void acceptFriend(client_accept_friend_t *payload);
+    static void acceptFriend(client_accept_friend_t *payload, SharedData& data);
 
-    static void friendStatus(client_friend_status_t *);
+    static void friendStatus(client_friend_status_t *, SharedData& data);
 };
 
 #endif //BABEL_SERVER_REQUESTHANDLER_HPP
