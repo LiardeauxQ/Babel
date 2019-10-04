f2a13c083aef2cf261f7d5466dbba1b82f6ad11f
Quentin <quentin.liardeaux@epitech.eu>
2019-10-04 15:16:11 +0200

    :hammer: Rework ServerRequest class, now it create Message data structur for every requests instead of handle server connection

--- Client/protocol/ServerRequest.cpp
+++ Client/protocol/ServerRequest.cpp
@@ -4,259 +4,170 @@
 
 #include "ServerRequest.hpp"
 
-boost::shared_ptr<ServerRequest> ServerRequest::create(const std::string &ipAddress, int port, boost::shared_ptr<NotificationHandler> notifHandler)
+static std::vector<std::tuple<int, Message (*)(std::map<std::string, void*>)>> requests = {
+        std::make_tuple(CLIENT_PING, &ServerRequest::createPingRequest),
+        std::make_tuple(CLIENT_HELLO, &ServerRequest::createHelloRequest),
+        std::make_tuple(CLIENT_GOODBYE, &ServerRequest::createGoodbyeRequest),
+        std::make_tuple(CLIENT_FRIEND_STATUS, &ServerRequest::createFriendStatusRequest),
+        std::make_tuple(CLIENT_REGISTER, &ServerRequest::createRegisterRequest),
+        std::make_tuple(CLIENT_FRIEND_REQUEST, &ServerRequest::createFriendRequest),
+        std::make_tuple(CLIENT_CALL, &ServerRequest::createCallRequest),
+        std::make_tuple(CLIENT_BYE, &ServerRequest::createByeRequest),
+        std::make_tuple(CLIENT_ACCEPT_FRIEND, &ServerRequest::createAcceptFriendRequest),
+};
+
+Message ServerRequest::createRequest(int id, std::map<std::string, void*> userInfo)
 {
-    return boost::shared_ptr<ServerRequest>(new ServerRequest(ipAddress, port, notifHandler));
+    for (auto request : requests) {
+        if (std::get<0>(request) == id)
+            return std::get<1>(request)(userInfo);
+    }
+    throw "Invalid id";
 }
 
-ServerRequest::ServerRequest(const std::string &ipAddress, int port, boost::shared_ptr<NotificationHandler> notifHandler) :
-    notifHandler_(notifHandler)
-    , context_()
-    , socket_(BoostTcp::socket(context_))
-    , ipAddress_(ipAddress)
-    , port_(port)
+Message ServerRequest::createPingRequest(std::map<std::string, void*> userInfo)
 {
-    subjects_.push_back(boost::shared_ptr<Subject>(new Subject("loginResponse")));
-    subjects_.push_back(boost::shared_ptr<Subject>(new Subject("registerResponse")));
-}
+    auto it = userInfo.find("time");
 
-ServerRequest::~ServerRequest()
-{
-    socket_.close();
-}
+    if (it == userInfo.end())
+        throw "Cannot find time";
+    time_t time = *(time_t*)(it->second);
 
-void ServerRequest::start()
-{
-    socket_.connect(BoostTcp::endpoint(
-            boost::asio::ip::address::from_string(ipAddress_), port_));
-    context_.run();
-}
-
-void ServerRequest::sendPing(time_t timeSent)
-{
-    client_ping_t clt = {timeSent};
+    client_ping_t clt = {time};
     Message message(CLIENT_PING, CLIENT_PING_SIZE, &clt);
 
-    sendData(message);
+    return message;
 }
 
-void ServerRequest::pingResponse(server_ping_response_t *srv)
+Message ServerRequest::createHelloRequest(std::map<std::string, void*> userInfo)
 {
+    auto userIt = userInfo.find("username");
+    auto passIt = userInfo.find("password");
 
-}
+    if (userIt == userInfo.end())
+        throw "Cannot find username key";
+    else if (passIt == userInfo.end())
+        throw "Cannot find password key";
 
-void ServerRequest::sendHello(const std::string &username, const std::string &password)
-{
+    char *username = (char*)(userIt->second);
+    char *password = (char*)(passIt->second);
     client_hello_t clt;
 
-    if (username.size() < USERNAME_LEN)
-        strcpy(clt.username, username.c_str());
-    if (password.size() < PASSWORD_LEN)
-        strcpy(clt.password, password.c_str());
+    if (strlen(username) < USERNAME_LEN)
+        strcpy(clt.username, username);
+    if (strlen(password) < PASSWORD_LEN)
+        strcpy(clt.password, password);
 
     Message message(CLIENT_HELLO, CLIENT_HELLO_SIZE, &clt);
 
-    sendData(message);
+    return message;
 }
 
-void ServerRequest::helloResponse(server_hello_response_t *srv)
-{
-    std::map<std::string, void*> userInfo;
-
-    userInfo.insert(std::pair<std::string, void*>(std::string("type"),
-            (void*)(std::string("result").c_str())));
-    userInfo.insert(std::pair<std::string, void*>(std::string("result"),
-            (void*)(&srv->result)));
-    for (auto sub : subjects_) {
-        if (sub->getLabel() == "loginResponse") {
-            sub->notify(userInfo);
-            break;
-        }
-    }
-}
-
-void ServerRequest::sendGoodbye()
+Message ServerRequest::createGoodbyeRequest(std::map<std::string, void*> userInfo)
 {
     client_goodbye_t clt;
-
     Message message(CLIENT_GOODBYE, CLIENT_GOODBYE_SIZE, &clt);
 
-    sendData(message);
+    return message;
 }
 
-void ServerRequest::sendFriendStatus()
+Message ServerRequest::createFriendStatusRequest(std::map<std::string, void*> userInfo)
 {
     client_friend_status_t clt;
     Message message(CLIENT_FRIEND_STATUS, CLIENT_FRIEND_STATUS_SIZE, &clt);
 
-    sendData(message);
+    return message;
 }
 
-void ServerRequest::sendRegister(const std::string &username, const std::string &password)
+Message ServerRequest::createRegisterRequest(std::map<std::string, void*> userInfo)
 {
+    auto userIt = userInfo.find("username");
+    auto passIt = userInfo.find("password");
+
+    if (userIt == userInfo.end())
+        throw "Cannot find username key";
+    else if (passIt == userInfo.end())
+        throw "Cannot find password key";
+
+    char *username = (char*)(userIt->second);
+    char *password = (char*)(passIt->second);
     client_register_t clt;
 
-    if (username.size() < USERNAME_LEN)
-        strcpy(clt.username, username.c_str());
-    if (password.size() < PASSWORD_LEN)
-        strcpy(clt.password, password.c_str());
+    if (strlen(username) < USERNAME_LEN)
+        strcpy(clt.username, username);
+    if (strlen(password) < PASSWORD_LEN)
+        strcpy(clt.password, password);
 
     Message message(CLIENT_REGISTER, CLIENT_REGISTER_SIZE, &clt);
 
-    sendData(message);
+    return message;
 }
 
-void ServerRequest::registerResponse(server_register_response_t *srv)
+Message ServerRequest::createFriendRequest(std::map<std::string, void*> userInfo)
 {
-    std::map<std::string, void*> userInfo;
+    auto userIt = userInfo.find("username");
 
-    userInfo.insert(std::pair<std::string, void*>(std::string("type"),
-            (void*)(std::string("result").c_str())));
-    userInfo.insert(std::pair<std::string, void*>(std::string("result"),
-            (void*)(&srv->result)));
-    for (auto sub : subjects_) {
-        if (sub->getLabel() == "registerResponse") {
-            sub->notify(userInfo);
-            break;
-        }
-    }
-}
+    if (userIt == userInfo.end())
+        throw "Cannot find username key";
 
-void ServerRequest::sendFriendRequest(const std::string &username)
-{
+    char *username = (char*)(userIt->second);
     client_friend_request_t clt;
 
-    if (username.size() < USERNAME_LEN)
-        strcpy(clt.username, username.c_str());
+    if (strlen(username) < USERNAME_LEN)
+        strcpy(clt.username, username);
 
     Message message(CLIENT_FRIEND_REQUEST, CLIENT_FRIEND_REQUEST_SIZE, &clt);
 
-    sendData(message);
+    return message;
 }
 
-void ServerRequest::sendCall(const std::vector<std::string> &usernames)
+Message ServerRequest::createCallRequest(std::map<std::string, void*> userInfo)
 {
-    client_call_t clt;
-    int i = 0;
+    auto usersIt = userInfo.find("usernames");
+    auto countIt = userInfo.find("count");
 
-    for (const auto username : usernames) {
-        if (i > MAX_FRIENDS)
-            break;
-        if (username.size() < USERNAME_LEN)
-            strcpy(clt.usernames[i++], username.c_str());
+    if (usersIt == userInfo.end())
+        throw "Cannot find usernames key";
+    else if (countIt == userInfo.end())
+        throw "Cannot find count key";
+
+    char **usernames = (char**)(usersIt->second);
+    int count = *(int*)(countIt->second);
+    client_call_t clt;
+
+    for (int i = 0 ; i < count && i < MAX_FRIENDS ; i++) {
+        if (strlen(usernames[i]) < USERNAME_LEN)
+            strcpy(clt.usernames[i++], usernames[i]);
     }
 
     Message message(CLIENT_CALL, CLIENT_CALL_SIZE, &clt);
 
-    sendData(message);
+    return message;
 }
 
-void ServerRequest::callResponse(server_call_response_t *srv)
-{
-
-}
-
-void ServerRequest::sendBye()
+Message ServerRequest::createByeRequest(std::map<std::string, void*> userInfo)
 {
     client_bye_t clt;
 
     Message message(CLIENT_BYE, CLIENT_BYE_SIZE, &clt);
 
-    sendData(message);
+    return message;
 }
 
-void ServerRequest::byeResponse(server_bye_response_t *srv)
+Message ServerRequest::createAcceptFriendRequest(std::map<std::string, void*> userInfo)
 {
+    auto messageIt = userInfo.find("message");
 
-}
+    if (messageIt == userInfo.end())
+        throw "Cannot find message key";
 
-void ServerRequest::sendAccecptFriend(const std::string &message)
-{
+    char *message = (char*)(messageIt->second);
     client_accept_friend_t clt;
 
-    if (message.size() < MESSAGE_LEN)
-        strcpy(clt.message, message.c_str());
+    if (strlen(message) < USERNAME_LEN)
+        strcpy(clt.message, message);
 
     Message sendingMsg(CLIENT_ACCEPT_FRIEND, CLIENT_ACCEPT_FRIEND_SIZE, &clt);
 
-    sendData(sendingMsg);
-}
-
-void ServerRequest::sendData(Message &message)
-{
-    boost::asio::async_write(
-            socket_,
-            boost::asio::buffer(message.getData(), message.getTotalSize()),
-            boost::bind(&ServerRequest::waitForResponse, shared_from_this(), boost::asio::placeholders::error));
-}
-
-void ServerRequest::waitForResponse(const boost::system::error_code &ec)
-{
-    std::cout << "waiting" << std::endl;
-    boost::asio::async_read(
-            socket_,
-            boost::asio::buffer(response_.getHeaderRaw(), HEADER_SIZE),
-            boost::bind(&ServerRequest::receivePacket, shared_from_this(), boost::asio::placeholders::error));
-}
-
-void ServerRequest::receivePacket(const boost::system::error_code &ec)
-{
-    if (!ec) {
-        std::cout << "packet" << std::endl;
-        response_.setupPayload();
-        boost::asio::async_read(
-                socket_,
-                boost::asio::buffer(response_.getPayload(), response_.getPayloadSize()),
-                boost::bind(&ServerRequest::receiveBody, shared_from_this(), boost::asio::placeholders::error));
-    } else {
-        std::cerr << "Error while receiving packet." << ec.message() << std::endl;
-    }
-}
-
-void ServerRequest::receiveBody(const boost::system::error_code &ec)
-{
-    if (!ec) {
-        handleResponse(response_);
-    } else {
-        std::cerr  << "Error while receiving body." << ec.message() << std::endl;
-    }
-}
-
-void ServerRequest::handleResponse(Message &response)
-{
-    std::cout << "Response" << std::endl;
-    switch (response.getId()) {
-        case SERVER_PING_RESPONSE:
-            pingResponse((server_ping_response_t*)(response.getPayload()));
-            break;
-        case SERVER_HELLO_RESPONSE:
-            helloResponse((server_hello_response_t*)(response.getPayload()));
-            break;
-        case SERVER_GOODBYE_RESPONSE:
-            byeResponse((server_bye_response_t*)(response.getPayload()));
-            break;
-        case SERVER_REGISTER_RESPONSE:
-            registerResponse((server_register_response_t*)(response.getPayload()));
-            break;
-        case SERVER_CALL_RESPONSE:
-            callResponse((server_call_response_t*)(response.getPayload()));
-            break;
-        default:
-            break;
-    }
-}
-
-std::string ServerRequest::receiveFriendRequest()
-{
-    return std::string("");
-}
-
-void ServerRequest::receiveCall()
-{
-
-}
-
-void ServerRequest::receiveBye()
-{
-
+    return sendingMsg;
 }
--- Client/protocol/ServerRequest.hpp
+++ Client/protocol/ServerRequest.hpp
@@ -7,65 +7,25 @@
 
 #include <string>
 #include <vector>
-#include <boost/asio.hpp>
-#include <boost/bind.hpp>
-#include <boost/enable_shared_from_this.hpp>
-#include <iostream>
+#include <tuple>
 #include <time.h>
 
 #include "Message.hpp"
 #include "protocol.h"
-#include "../NotificationHandler.hpp"
 
-typedef boost::asio::ip::tcp BoostTcp;
-
-class ServerRequest
-    : public boost::enable_shared_from_this<ServerRequest> {
+class ServerRequest {
 public:
-    ServerRequest(const std::string &ipAddress, int port,
-            boost::shared_ptr<NotificationHandler> notifHandler);
+    static Message createRequest(int id, std::map<std::string, void*> userInfo);
 
-    ~ServerRequest();
-
-    static boost::shared_ptr<ServerRequest> create(const std::string &ipAddress, int port,
-            boost::shared_ptr<NotificationHandler> notifHandler);
-
-    void start();
-
-    void sendPing(time_t timeSent);
-    void sendHello(const std::string &username, const std::string &password);
-    void sendGoodbye();
-    void sendFriendStatus();
-    void sendRegister(const std::string &username, const std::string &password);
-    void sendFriendRequest(const std::string &username);
-    void sendCall(const std::vector<std::string> &usernames);
-    void sendBye();
-    void sendAccecptFriend(const std::string &message);
-
-    std::string receiveFriendRequest();
-    void receiveCall();
-    void receiveBye();
-
-private:
-    void pingResponse(server_ping_response_t *srv);
-    void helloResponse(server_hello_response_t *srv);
-    void registerResponse(server_register_response_t *srv);
-    void callResponse(server_call_response_t *srv);
-    void byeResponse(server_bye_response_t *srv);
-
-    void sendData(Message &message);
-    void waitForResponse(const boost::system::error_code &ec);
-    void receivePacket(const boost::system::error_code &ec);
-    void receiveBody(const boost::system::error_code &ec);
-    void handleResponse(Message &message);
-
-    boost::shared_ptr<NotificationHandler> notifHandler_;
-    boost::asio::io_context context_;
-    BoostTcp::socket socket_;
-    std::string ipAddress_;
-    Message response_;
-    int port_;
-    std::vector<boost::shared_ptr<Subject>> subjects_;
+    static Message createPingRequest(std::map<std::string, void*> userInfo);
+    static Message createHelloRequest(std::map<std::string, void*> userInfo);
+    static Message createGoodbyeRequest(std::map<std::string, void*> userInfo);
+    static Message createFriendStatusRequest(std::map<std::string, void*> userInfo);
+    static Message createRegisterRequest(std::map<std::string, void*> userInfo);
+    static Message createFriendRequest(std::map<std::string, void*> userInfo);
+    static Message createCallRequest(std::map<std::string, void*> userInfo);
+    static Message createByeRequest(std::map<std::string, void*> userInfo);
+    static Message createAcceptFriendRequest(std::map<std::string, void*> userInfo);
 };
 
 
