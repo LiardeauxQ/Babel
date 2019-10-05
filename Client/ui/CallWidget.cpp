0488349b0cc3fdaa9dd30b9e68051b653a67c6c4
Quentin <quentin.liardeaux@epitech.eu>
2019-10-05 22:41:06 +0200

    :zap: Connect CallWidget to server request

--- Client/ui/CallWidget.cpp
+++ Client/ui/CallWidget.cpp
@@ -4,7 +4,10 @@
 
 #include "CallWidget.hpp"
 
-ui::CallWidget::CallWidget(QWidget *parent) : QWidget(parent)
+ui::CallWidget::CallWidget(boost::shared_ptr<NotificationHandler> notifHandler, QWidget *parent) :
+    QWidget(parent),
+    notifHandler_(notifHandler),
+    callEvent_(new Subject("call"))
 {
     closeButton_ = QSharedPointer<QPushButton>(new QPushButton("Close"));
     startCallButton_ = QSharedPointer<QPushButton>(new QPushButton("Start"));
@@ -23,6 +26,13 @@ ui::CallWidget::CallWidget(QWidget *parent) : QWidget(parent)
     mainLayout->addWidget(stopCallButton_.get());
 
     setLayout(mainLayout);
+    notifHandler->registerEvent(callEvent_);
+}
+
+void ui::CallWidget::setFriendUsername(const std::string &username)
+{
+    friendUsername_ = username;
+    friendUsernameLabel_->setText(QString("Do you want to call ") + QString(friendUsername_.c_str()));
 }
 
 void ui::CallWidget::closeTap()
@@ -37,11 +47,18 @@ void ui::CallWidget::closeTap()
 
 void ui::CallWidget::startTap()
 {
-    std::cout << "start" << std::endl;
+    std::map<std::string, void*> userInfo;
+
+    isCalling_ = true;
+    friendUsernameLabel_->setText(QString("Calling ") + QString(friendUsername_.c_str()));
+    userInfo["type"] = (void*)(std::string("call").c_str());
+    userInfo["username"] = (void*)(friendUsername_.c_str());
+    callEvent_->notify(userInfo);
 }
 
 void ui::CallWidget::stopTap()
 {
+    isCalling_ = false;
     std::cout << "stop" << std::endl;
 }
 
--- Client/ui/CallWidget.hpp
+++ Client/ui/CallWidget.hpp
@@ -8,24 +8,33 @@
 #include <QtWidgets>
 #include <QSharedPointer>
 #include <iostream>
+#include <boost/shared_ptr.hpp>
+
+#include "../NotificationHandler.hpp"
 
 namespace ui {
     class CallWidget : public QWidget {
     Q_OBJECT
     public:
-        explicit CallWidget(QWidget *parent = nullptr);
+        explicit CallWidget(boost::shared_ptr<NotificationHandler> notifHandler, QWidget *parent = nullptr);
+
+        void setFriendUsername(const std::string &username);
     private:
         QSharedPointer<QPushButton> closeButton_;
         QSharedPointer<QPushButton> startCallButton_;
         QSharedPointer<QPushButton> stopCallButton_;
         QSharedPointer<QLabel> friendUsernameLabel_;
+        boost::shared_ptr<NotificationHandler> notifHandler_;
 
+        boost::shared_ptr<Subject> callEvent_;
+
+        bool isCalling_;
+        std::string friendUsername_;
     private slots:
         void closeTap();
         void startTap();
         void stopTap();
     };
 }
-
 
 #endif //BABEL_SERVER_CALLWIDGET_HPP
