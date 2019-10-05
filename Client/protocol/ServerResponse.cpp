4f814b49aa9f9695bdc53477722758a2a0423317
Quentin <quentin.liardeaux@epitech.eu>
2019-10-05 22:39:09 +0200

    :sparkles: Recreate ServerResponse to handle response from server asynchronously

---
+++ Client/protocol/ServerResponse.cpp
@@ -0,0 +1,129 @@
+//
+// Created by Quentin Liardeaux on 10/5/19.
+//
+
+#include "ServerResponse.hpp"
+
+#include <QThread>
+
+ServerResponse::ServerResponse(boost::shared_ptr<std::queue<Message>> responses,
+        boost::shared_ptr<boost::mutex> mutex,
+        boost::shared_ptr<NotificationHandler> notifHandler) :
+        QObject(nullptr),
+        responses_(responses),
+        mutex_(mutex),
+        notifHandler_(notifHandler)
+{}
+
+bool ServerResponse::event(QEvent *event)
+{
+    mutex_->lock();
+    Message message = responses_->front();
+    dispatchPayloads(message);
+    responses_->pop();
+    mutex_->unlock();
+    std::cout << "event has been triggered" << QThread::currentThread() << std::endl;
+    return true;
+}
+
+void ServerResponse::dispatchPayloads(Message message)
+{
+    switch (message.getId()) {
+        case SERVER_HELLO_RESPONSE:
+            hello(message.getPayload());
+            break;
+        case SERVER_REGISTER_RESPONSE:
+            registerResponse(message.getPayload());
+            break;
+        case SERVER_FRIEND_STATUS:
+            fetchFriends(message.getPayload());
+            break;
+        case SERVER_CALL_RESPONSE:
+            call(message.getPayload());
+            break;
+        default:
+            break;
+    }
+}
+
+void ServerResponse::notifyObservers(const std::string &label, std::map<std::string, void *> userInfo)
+{
+    for (auto sub : subjects_) {
+        if (sub->getLabel() == label) {
+            sub->notify(userInfo);
+            break;
+        }
+    }
+}
+
+void ServerResponse::hello(void *payload)
+{
+    static bool isInit = false;
+
+    std::cout << "std::endl" << std::endl;
+    if (!isInit) {
+        subjects_.push_back(boost::shared_ptr<Subject>(new Subject("loginResponse")));
+        notifHandler_->registerEvent(subjects_.back());
+        isInit = true;
+    }
+    std::cout << "std::endl" << std::endl;
+    server_hello_response_t *srv = (server_hello_response_t*)payload;
+    std::map<std::string, void*> userInfo;
+
+    userInfo["type"] = (void*)(std::string("result").c_str());
+    userInfo["result"] = (void*)(&srv->result);
+    std::cout << "std::endl" << std::endl;
+    notifyObservers("loginResponse", userInfo);
+}
+
+void ServerResponse::registerResponse(void *payload)
+{
+    static bool isInit = false;
+
+    if (!isInit) {
+        subjects_.push_back(boost::shared_ptr<Subject>(new Subject("registerResponse")));
+        notifHandler_->registerEvent(subjects_.back());
+        isInit = true;
+    }
+    server_register_response_t *srv = (server_register_response_t*)payload;
+    std::map<std::string, void*> userInfo;
+
+    userInfo["type"] = (void*)(std::string("result").c_str());
+    userInfo["result"] = (void*)(&srv->result);
+    notifyObservers("registerResponse", userInfo);
+}
+
+void ServerResponse::fetchFriends(void *payload)
+{
+    static bool isInit = false;
+
+    if (!isInit) {
+        subjects_.push_back(boost::shared_ptr<Subject>(new Subject("fetchFriendsResponse")));
+        notifHandler_->registerEvent(subjects_.back());
+        isInit = true;
+    }
+    server_friend_status_t *srv = (server_friend_status_t*)payload;
+    std::map<std::string, void*> userInfo;
+
+    userInfo["type"] = (void*)(std::string("result").c_str());
+    userInfo["payload"] = payload;
+    notifyObservers("fetchFriendsResponse", userInfo);
+}
+
+void ServerResponse::call(void *payload)
+{
+    static bool isInit = false;
+
+    if (!isInit) {
+        subjects_.push_back(boost::shared_ptr<Subject>(new Subject("callResponse")));
+        notifHandler_->registerEvent(subjects_.back());
+        isInit = true;
+    }
+    server_friend_status_t *srv = (server_friend_status_t*)payload;
+    std::map<std::string, void*> userInfo;
+
+    userInfo["type"] = (void*)(std::string("result").c_str());
+    userInfo["payload"] = payload;
+    std::cout << "Call is accepted" << std::endl;
+    notifyObservers("callResponse", userInfo);
+}
---
+++ Client/protocol/ServerResponse.hpp
@@ -0,0 +1,44 @@
+//
+// Created by Quentin Liardeaux on 10/5/19.
+//
+
+#ifndef BABEL_SERVER_SERVERRESPONSE_HPP
+#define BABEL_SERVER_SERVERRESPONSE_HPP
+
+#include <QObject>
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <tuple>
+#include <boost/shared_ptr.hpp>
+#include <boost/thread.hpp>
+
+#include "../NotificationHandler.hpp"
+#include "Message.hpp"
+
+class ServerResponse: public QObject {
+    Q_OBJECT
+public:
+    ServerResponse(boost::shared_ptr<std::queue<Message>> responses,
+            boost::shared_ptr<boost::mutex> mutex,
+            boost::shared_ptr<NotificationHandler> notifHandler);
+    ~ServerResponse() = default;
+protected:
+    bool event(QEvent *event);
+private:
+
+    void hello(void *payload);
+    void registerResponse(void *payload);
+    void fetchFriends(void *payload);
+    void call(void *payload);
+    void notifyObservers(const std::string &label, std::map<std::string, void*> userInfo);
+    void initHandler();
+    void dispatchPayloads(Message message);
+
+    boost::shared_ptr<std::queue<Message>> responses_;
+    boost::shared_ptr<boost::mutex> mutex_;
+    boost::shared_ptr<NotificationHandler> notifHandler_;
+    std::vector<boost::shared_ptr<Subject>> subjects_;
+};
+
+#endif //BABEL_SERVER_SERVERRESPONSE_HPP
